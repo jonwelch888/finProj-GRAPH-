d0f635598f0afa3611e1ec319d83347d646cdb65
jonwelch888 <jon.welch@g.austincc.edu>
2024-07-27 06:06:47 -0500

    attempting to build test for graph traversals ;
    bool compareVectors(const std::vector<int>& v1, const std::vector<int>& v2)
    {
        return v1 == v2;
    }

--- function.h
+++ function.h
@@ -1,16 +1,20 @@
 /***************************************************
 * Name: Jon Welch
 * Assignment: Final Project
-* Purpose of the file: This header file declares the int random_range function.
+* Purpose of the file: This header file declares extra helper function for testing; 
 * 
 * @note This file is associated with [function.cpp], & [main.cpp];
 ****************************************************/
 
 #include <stdexcept>
 #include <stdlib.h> /* rand */
+#include <vector>
+#include <algorithm>
 
 #ifndef FUNCTION_H
 #define FUNCTION_H
+
+bool compareVectors(const std::vector<int>&, const std::vector<int>&);
 
 int random_range(int, int);
 
