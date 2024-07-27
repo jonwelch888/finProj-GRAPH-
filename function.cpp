f97ed190ab62416952c68c7b2e452756eb41bd6b
jonwelch888 <jon.welch@g.austincc.edu>
2024-07-27 02:36:00 -0500

    1st attempt at function.cpp

--- function.cpp
+++ function.cpp
@@ -1,8 +1,23 @@
 /*********************************************
 * Name: Jon Welch
 * Assignment: Final Project
-* Purpose of the file: This source file contains the implementation of the 
-random_range function.
+* Purpose of the file: This source file contains the implementation of the random_range function.
 * 
 * @note This file is associated with [function.h]
 *********************************************/
+
+#include "function.h"
+#include <cstdlib>
+
+int random_range(int min, int max)
+{
+    /*********************************************
+    * random_range: Function to generate a random integer within a specified range.
+    * @param min : minimum value of the range
+    * @param max : maximum value of the range
+    * @return : random integer within the range [min, max]
+    *********************************************/
+    return (rand() % (max - min + 1)) + min;
+}
+
+
