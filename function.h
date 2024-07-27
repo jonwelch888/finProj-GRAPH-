57dc2770b8ad2418b8da84eae0b672018d36b07c
jonwelch888 <jon.welch@g.austincc.edu>
2024-07-27 02:24:40 -0500

    1st attempt at creating function.h
    coming back to this, and cross reference with some previous assignments; make sure that I am structuring this properly.

--- function.h
+++ function.h
@@ -1,7 +1,14 @@
 /***************************************************
 * Name: Jon Welch
 * Assignment: Final Project
-* Purpose of the file: This header file declares the random_range function.
+* Purpose of the file: This header file declares the int random_range function.
 * 
-* @note This file is associated with [function.cpp],& [main.cpp];
+* @note This file is associated with [function.cpp], & [main.cpp];
 ****************************************************/
+
+#ifndef RANDOM_RANGE_H
+#define RANDOM_RANGE_H
+
+int random_range(int, int);
+
+#endif // RANDOM_RANGE_H
