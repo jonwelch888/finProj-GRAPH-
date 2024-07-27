e66b10c206e1a0d494f6ed591afba9d769049cc4
jonwelch888 <jon.welch@g.austincc.edu>
2024-07-27 02:21:27 -0500

    1st attempt at creating main.h

--- main.h
+++ main.h
@@ -1,9 +1,19 @@
 /*********************************************
 * Name: Jon Welch
 * Assignment: Final Project
-* Purpose of the file: This header file includes the necessary standard 
-library headers
-* and declares the random_range function used in main.cpp.
+* Purpose of the file: This header file includes the necessary standard library headers
 *
-* @note This file is associated with [main.cpp]
+* @note This file is associated with [main.cpp], [graph.h], & [function.h];
 *********************************************/
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#include "graph.h"
+#include "function.h"
+#include <iostream>
+#include <ctime>
+#include <cassert>
+
+#endif // MAIN_H
+
